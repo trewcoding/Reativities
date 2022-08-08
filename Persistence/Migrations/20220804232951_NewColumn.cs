18f07e3c8737635b42b2b95f5d2efa2cf89fd1de
James Trewin <james.trewin@cba.tech>
2022-08-08 11:45:37 +1000

    Section 2

---
+++ Persistence/Migrations/20220804232951_NewColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class NewColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Desciption",
+                table: "Activities",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Activities",
+                newName: "Desciption");
+        }
+    }
+}
